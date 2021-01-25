f15b67ba127984a73d1433c0a7eb7fb43a328e1a
Alexander Neville <alexander.neville@icloud.com>
2021-01-25 11:30:45 +0000

    fixed minor bug, displaying messages from others twice on page load.

--- templates/right_pane.php
+++ templates/right_pane.php
@@ -6,6 +6,8 @@ $chat_rooms = $current_user->get_chat_rooms();
 for ($i = 0; $i < count($chat_rooms); $i++) {
 
     render_room($chat_rooms[$i]);
+    $timestamp = date("Y-m-d H:i:s");
+    $current_user->set_last_online($chat_rooms[$i]->get_id(), $timestamp);
 
 }
 ?>
